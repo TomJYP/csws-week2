c08b601c78fddc3d6d462d0d27cdd64238563880
Tom Palmer <thomasjy2002@gmail.com>
2023-01-27 11:58:20 +0000

    lab2

---
+++ lab2.py
@@ -0,0 +1,8 @@
+"""
+locations = ["Sweden", "Japan", "Norway", "New York", "Beijing"]
+locations.sort(reverse=True)
+print(locations)
+"""
+cities = ["Tokyo", "Kyoto", "Osaka", "Fukouka", "Hiroshima"]
+len(cities)
+print(cities)
